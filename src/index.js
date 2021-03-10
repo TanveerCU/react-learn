6d9ab346961a20ea3d4dbb9fda34a6e18ef976eb
tanveer <atanveer712@gmail.com>
2021-03-10 12:47:21 +0600

    new commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+let speech = "Good Morning";
+let color = {color:"gray"};
+const time = new Date(2021,3,10,18);
+
+  if(time.getHours() <= 12){
+    color.color = "gray"
+  }else if(time.getHours() > 12 &&  time.getHours() <= 15){
+    speech = "Good noon";
+    color.color = "yellow"
+  }
+  else if(time.getHours() >15  &&  time.getHours() <= 18 ){
+    speech = "Good afternoon";
+    color.color = "red"
+  }
+  else if(time.getHours() >18){
+    speech = "Good night";
+    color.color = "black"
+  }
+
 
 ReactDOM.render(
   <>
-  <h1>Netflix Pick</h1>
-  <p>List of five best Series:</p>
-  <ol>
-    <li>movie 01</li>
-    <li>movie 02</li>
-    <li>movie 03</li>
-    <li>movie 04</li>
-    <li>movie 05</li>
-  </ol>
-  </>,
+  <div className="container">
+    <span className="first">Hello sir, </span>
+    <span style={color}>{speech}</span>
+  </div>
+  </> ,
   document.getElementById('root')
 );
